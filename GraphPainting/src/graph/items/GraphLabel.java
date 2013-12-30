e6ac9e443903e6ac24c1a1884c68ab872d8b7a91
Benjamin Sigg <benjamin_sigg@gmx.ch>
2013-12-30 20:19:26 +0100

    Formatting the code

--- GraphPainting/src/graph/items/GraphLabel.java
+++ GraphPainting/src/graph/items/GraphLabel.java
@@ -14,54 +14,52 @@ import javax.swing.JLabel;
 
 public class GraphLabel extends MoveableRectangularGraphItem implements GraphItem, Regraphable, Rectangular {
 	private JLabel label;
-	
-	public GraphLabel( String text ){
+
+	public GraphLabel( String text ) {
 		label = new JLabel();
-		
-        label.setMinimumSize(new Dimension(5, 5));
-        setBoundaries(0, 0, 5, 5);
-        
+
+		label.setMinimumSize( new Dimension( 5, 5 ) );
+		setBoundaries( 0, 0, 5, 5 );
+
 		label.setText( text );
 		label.setOpaque( true );
 		label.setBackground( new Color( 128, 128, 255 ) );
-		label.setBorder( BorderFactory.createCompoundBorder( 
-				BorderFactory.createLineBorder( Color.BLACK, 1 ), 
-				BorderFactory.createEmptyBorder( 2, 2, 2, 2 ) ) );
+		label.setBorder( BorderFactory.createCompoundBorder( BorderFactory.createLineBorder( Color.BLACK, 1 ), BorderFactory.createEmptyBorder( 2, 2, 2, 2 ) ) );
 	}
-	
+
 	public JLabel getLabel() {
 		return label;
 	}
-	
-	public void setLocation( int x, int y ){
+
+	public void setLocation( int x, int y ) {
 		Dimension size = label.getPreferredSize();
 		setBoundaries( x, y, size.width, size.height );
 	}
-	
+
 	@Override
 	public void setBoundaries( Rectangle boundaries ) {
 		label.setBounds( boundaries );
 		super.setBoundaries( boundaries );
 	}
-	
+
 	@Override
 	protected void addTo( GraphSite site ) {
 		site.addComponent( label );
-		site.addRegraphable( (Regraphable)this );
+		site.addRegraphable( (Regraphable) this );
 		super.addTo( site );
 	}
-	
+
 	@Override
 	protected void removeFrom( GraphSite site ) {
 		site.removeComponent( label );
-		site.removeRegraphable( (Regraphable)this );
+		site.removeRegraphable( (Regraphable) this );
 		super.removeFrom( site );
 	}
-	
+
 	@Override
 	public void regraphed() {
 		Rectangle bounds = getBoundaries();
 		bounds.setSize( label.getPreferredSize() );
-		setBoundaries( bounds );	
+		setBoundaries( bounds );
 	}
 }
